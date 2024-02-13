fd774d197422bfd3034a5b12ceca6f06831a9550
Ma, Tzu-Yu <122843515+TzuYuMa@users.noreply.github.com>
2024-02-13 00:20:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ import json
 
 app = Flask(__name__) # setup initial flask app; gets called throughout in routes
 
-# Connect to the PostgreSQL database
-
-pgSQL_connect = {
-    'dbname':"gis5572",
-    'user':"postgres",
-    'password':"19950920840920Yu",
-    'host':"34.133.74.255",
-    'port':"5432"
-}
-
 
 @app.route('/') #python decorator 
 def hello_world(): #function that app.route decorator references
@@ -27,36 +17,8 @@ def hello():
 @app.route('/getgeojson', methods=['GET'])
 def get_geojson():
     # Connect to the database
-    connection = psycopg2.connect(**pgSQL_connect)
-    cursor = connection.cursor()
-
-    # Query to retrieve polygon as GeoJSON
-    query = "SELECT ST_AsGeoJSON(geometry) FROM polygon_lab1;"
-    cursor.execute(query)
-    rows = cursor.fetchall()
-
-    # Close database connection
-    cursor.close()
-    connection.close()
-
-    # Prepare GeoJSON response
-    features = []
-    for row in rows:
-        feature = {
-            "type": "Feature",
-            "geometry": json.loads(row[0]),
-            "properties": {}
-        }
-        features.append(feature)
-
-    feature_collection = {
-        "type": "FeatureCollection",
-        "features": features
-    }
-
-    # Return GeoJSON response
-    return jsonify(feature_collection)
-
+    {'features': [{'geometry': {'coordinates': [[[25, 121.5], [25, 121], [25.5, 121.5], [25.5, 121], [25, 121.5]]], 'type': 'Polygon'}, 'properties': {}, 'type': 'Feature'}], 'type': 'FeatureCollection'}
+    
 if __name__ == "__main__":
     app.run(
       #debug=True, #shows errors 
